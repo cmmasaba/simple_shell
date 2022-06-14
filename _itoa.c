61758c19f48cbc73ae539dd68a8fff07cec18d05
Daniel Uboa <uboadaniel@gmail.com>
2022-06-13 23:10:53 -0700

    Task 0: Betty would be proud

---
+++ _itoa.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * convert - Converter function, a clone of itoa
+ *
+ * @num: Number
+ * @base: Base
+ *
+ * Return: String
+ */
+
+char *convert(long int num, int base)
+{
+	static char *array = "0123456789abcdef";
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (num < 0)
+	{
+		n = -num;
+		sign = '-';
+	}
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do {
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
+/**
+ * main - Check the code
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int a = 435;
+
+	printf("%s\n", convert(a, 10));
+	printf("%s\n", convert(a, 2));
+	printf("%s\n", convert(a, 16));
+}
